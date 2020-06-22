12632a9509054f7f7abc0047ec540681f14e05c7
Radu <radu.palacean@gmail.com>
2020-06-22 20:56:17 +0300

    Add files via upload

---
+++ 01_Copy wallpapers.bat
@@ -0,0 +1,5 @@
+echo
+xcopy /h "C:\Users\radu.palacean\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" "D:\Personal\Windows wallpapers\temp"
+set Pathname="D:\Personal\Windows wallpapers\temp"
+cd /d %Pathname%
+ren *.* *.jpg
