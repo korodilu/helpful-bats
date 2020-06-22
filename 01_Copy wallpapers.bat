ddd7e9d59f05a379e097138b92063d866661ceea
Radu <radu.palacean@gmail.com>
2020-06-22 21:15:19 +0300

    Removed initial file

--- 01_Copy wallpapers.bat
+++
@@ -1,5 +0,0 @@
-echo
-xcopy /h "C:\Users\radu.palacean\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" "D:\Personal\Windows wallpapers\temp"
-set Pathname="D:\Personal\Windows wallpapers\temp"
-cd /d %Pathname%
-ren *.* *.jpg
